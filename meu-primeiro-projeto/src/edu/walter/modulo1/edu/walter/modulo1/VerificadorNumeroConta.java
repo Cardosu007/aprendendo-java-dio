48de7e0b40b6f2fd9a3fbde0419c52c2e300770f
W Cardoso <cardosu7@gmail.com>
2024-04-27 10:19:13 -0300

    att

---
+++ meu-primeiro-projeto/src/edu/walter/modulo1/edu/walter/modulo1/VerificadorNumeroConta.java
@@ -0,0 +1,28 @@
+package edu.walter.modulo1;
+
+import java.util.Scanner;
+
+public class VerificadorNumeroConta {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            String numeroConta = scanner.nextLine();
+
+            verificarNumeroConta(numeroConta);
+
+            System.out.println("Numero de conta valido.");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Erro: Numero de conta invalido. Digite exatamente 8 digitos.");
+        } finally {
+            scanner.close();
+        }
+    }
+
+    private static void verificarNumeroConta(String numeroConta) {
+        if (numeroConta.length() != 8) {
+            throw new IllegalArgumentException("O numero da conta deve ter exatamente 8 digitos.");
+        }
+    }
+}
